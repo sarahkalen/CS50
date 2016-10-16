2beaa9fb783ec4690cf5a17c6f7e867b2a8fac98
Sarah Kalen <skalen@brynmawr.edu>
2016-10-16 18:26:31 -0400

    Caesar's Cipher

---
+++ caesarscipher.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    while (argc != 2) //check for one argc==2
+    {
+        printf("Please input a single argument");
+        return 1;
+    }
+    
+   string s = GetString();
+    int k = atoi(argv[1]);
+    
+    
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        if(s[i] == ' ')
+        {
+            printf(" "); //if space
+        }
+        else if(s[i] >= 'a'  && s[i] <= 'z') //if lowercase
+        {
+            int c = s[i] - 97; //0-25 scale
+            int shift = (c + k)%26; 
+            printf("%c", shift + 97 );
+        }
+        else if(s[i] >= 'A'  && s[i] <= 'Z') //if uppercase
+        {
+            int c = s[i] - 65; //0-25 scale
+            int shift = (c + k)%26;  
+            printf("%c", shift + 65);
+        }
+        else
+        {
+        printf("%c", s[i]);
+        }
+    }
+     printf("\n");
+    }
+    
+    
